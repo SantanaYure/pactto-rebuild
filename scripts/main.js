7833dac16ccf20ecc8918c8597b8a6d9c1be9b31
SantanaYure <yure.s.santana@outlook.com>
2024-08-10 00:02:55 -0300

    crianção do header

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuIcon = document.querySelector('.menu-icon');
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
   const sidebar = document.querySelector('.sidebar');
+  const closeBtn = document.querySelector('.close-btn');
 
-  menuIcon.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-  });
-
-  function handleResize() {
-    if (window.innerWidth > 720) {
-      sidebar.classList.remove('active');
-    }
+  function toggleMenu() {
+    sidebar.classList.toggle('open');
   }
 
-  window.addEventListener('resize', handleResize);
+  menuToggle.addEventListener('click', toggleMenu);
+  closeBtn.addEventListener('click', toggleMenu);
+
+  // Fechar o menu ao clicar fora dele
+  document.addEventListener('click', function (event) {
+    if (!sidebar.contains(event.target) && !menuToggle.contains(event.target) && sidebar.classList.contains('open')) {
+      toggleMenu();
+    }
+  });
 });
