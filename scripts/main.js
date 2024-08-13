63c478c354a4e0a0d8d6781aea673338671dca49
SantanaYure <yure.s.santana@outlook.com>
2024-08-13 13:48:57 -0300

    Layout atualizado

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.querySelector('.menu-toggle');
   const sidebar = document.querySelector('.sidebar');
   const closeBtn = document.querySelector('.close-btn');
+  const loginIcon = document.querySelector('.login-icon');
+  const dropdown = document.querySelector('.dropdown');
 
   function toggleMenu() {
     sidebar.classList.toggle('open');
   }
 
+  function toggleDropdown() {
+    dropdown.classList.toggle('show');
+  }
+
   menuToggle.addEventListener('click', toggleMenu);
   closeBtn.addEventListener('click', toggleMenu);
+  loginIcon.addEventListener('click', toggleDropdown);
 
   // Fechar o menu ao clicar fora dele
   document.addEventListener('click', function (event) {
     if (!sidebar.contains(event.target) && !menuToggle.contains(event.target) && sidebar.classList.contains('open')) {
       toggleMenu();
+    }
+
+    if (!loginIcon.contains(event.target) && !dropdown.contains(event.target) && dropdown.classList.contains('show')) {
+      toggleDropdown();
     }
   });
 });
